c524688a8100a14cec82b6b56b4c0e835e01bf49
Shabhai Zafar <zfnanshabhai@gmail.com>
2026-08-11 13:52:45 +0530

    PR. 1 Fundamental Booster

---
+++ first.py
@@ -0,0 +1,89 @@
+# num1 = 2
+# num2 = 5
+
+
+# print("Number 1 : ",num1)
+# print("Number 2 : ",num2)
+
+# Arithmatic Operator: 
+# + - / * % // **
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
+# print(num1 // num2) # floor division
+# print(num1 % num2) 
+# print(num1**num2)   # power    2^5  2*2*2*2*2
+
+#========================================================
+# Assignment Operator : 
+# = += -= *= /= %=
+# num1 = 2
+# num2 = 5
+
+
+# print("Number 1 : ",num1)
+# print("Number 2 : ",num2)
+
+# num1+=num2  # num1 = num1+num2 = 2 + 5 = 7
+# num1-=num2   # num1 = num1 - num2 = 2-5 = -3
+
+# num1*=3  # num1 = num1 * 3 
+# num1/=3
+# num1%=3   # num1 = num1 % 3 = 2%3 = 2
+
+
+# print("Number 1 : ",num1)
+# print("Number 2 : ",num2)
+#========================================================
+# Relational Operator :  True/False
+# < > <= >= == !=
+
+# num1 = 10
+# num2 = 10
+
+# print(num1 < num2)  # false
+# print(num1 <= num2) # false   # true
+# print(num1 > num2) # true     # false
+# print(num1 >= num2) # true
+# print(num1 == num2) # false  # true
+# print(num1 != num2) # true   # false
+
+#===================================================
+# Logical Operator : 
+# and or not 
+# and :  return true if both eq are true else return false.
+# or :  return false if both eq are false else return true.
+
+# num1 = False
+# num2 = 10
+
+# print((num1 < num2) and (num1 == num2) )
+# print((num1 > num2) or (num1 != num2) )
+
+# print( num2 and num1 )
+# print( num2 or num1 )
+
+
+
+
+
+name = "Alice"
+
+print("Name:",name,type(name),"Memory Address:",id(name))
+
+
+
+
+
+
+
+
+
+
+# Comments : 
+# Single Line Comment
+'''
+sdfghj
+'''
+
